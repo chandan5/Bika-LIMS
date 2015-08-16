ced71387507bffecb23f177c7b3b74211e57df8f
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2015-08-17 02:43:24 +0530

    removed review_state and some indexes from createreport

--- bika/lims/reports/createreport.py
+++ bika/lims/reports/createreport.py
@@ -62,13 +62,7 @@ class CreateReport(object):
                 {'i': 'created',
                  'o': 'plone.app.querystring.operation.date.today'},
                 {'i': 'DatePublished',
-                 'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'cancellation_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
+                 'o': 'plone.app.querystring.operation.date.today'}
             ])
         elif report_type == 'productivity_analysespersampletype':
             alsoProvides(obj, IAnalysesPerSampleType)
@@ -76,25 +70,13 @@ class CreateReport(object):
             obj.Schema().getField('query').set(obj, [
                 {'i': 'created',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'cancellation_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         elif report_type == 'productivity_analysesperclient':
             alsoProvides(obj, IAnalysesPerClient)
             obj.base_query = {'portal_type': 'Analysis'}
             obj.Schema().getField('query').set(obj, [
                 {'i': 'created',
-                 'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'cancellation_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
+                 'o': 'plone.app.querystring.operation.date.today'}
             ])
         elif report_type == 'productivity_analysestats':
             alsoProvides(obj, IAnalysesStats)
@@ -103,12 +85,11 @@ class CreateReport(object):
                 {'i': 'ClientTitle',
                  'o': 'plone.app.querystring.operation.selection.is'},
                 {'i': 'DateReceived',
-                 'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
+                 'o': 'plone.app.querystring.operation.date.today'}
             ])
         elif report_type == 'productivity_analysestats_overtime':
             alsoProvides(obj, IAnalysesStatsOverTime)
+            obj.base_query = {'portal_type': 'Analysis'}
             obj.Schema().getField('query').set(obj, [
                 {'i': 'AnalysisService',
                  'o': 'plone.app.querystring.operation.selection.is'},
@@ -124,16 +105,12 @@ class CreateReport(object):
             obj.Schema().getField('query').set(obj, [
                 {'i': 'created',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         elif report_type == 'productivity_analysesperformedpertotal':
             alsoProvides(obj, IAnalysesPerformedPerTotal)
             obj.Schema().getField('query').set(obj, [
                 {'i': 'created',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         elif report_type == 'productivity_analysesattachments':
             alsoProvides(obj, IAnalysesAttachments)
@@ -155,24 +132,12 @@ class CreateReport(object):
             obj.Schema().getField('query').set(obj, [
                 {'i': 'DateReceived',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'cancellation_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         elif report_type == 'qualitycontrol_analysesrepeated':
             alsoProvides(obj, IAnalysesRepeated)
             obj.Schema().getField('query').set(obj, [
                 {'i': 'DateReceived',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'cancellation_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         elif report_type == 'qualitycontrol_resultspersamplepoint':
             alsoProvides(obj, IResultsPerSamplePoint)
@@ -187,8 +152,6 @@ class CreateReport(object):
                  'o': 'plone.app.querystring.operation.selection.is'},
                 {'i': 'DateSampled',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'worksheetanalysis_review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         # incomplete information about below 2 reports
         elif report_type == 'qualitycontrol_referenceanalysisqc':
@@ -208,10 +171,6 @@ class CreateReport(object):
             obj.Schema().getField('query').set(obj, [
                 {'i': 'DatePublished',
                  'o': 'plone.app.querystring.operation.date.today'},
-                {'i': 'review_state',
-                 'o': 'plone.app.querystring.operation.selection.is'},
-                {'i': 'cancellation_state',
-                 'o': 'plone.app.querystring.operation.selection.is'}
             ])
         elif report_type == 'administration_userhistory':
             alsoProvides(obj, IUserHistory)
